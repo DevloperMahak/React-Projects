355b21c4a8ecc0db92237f5feb3813da6b2ddd4c
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-06-09 20:07:29 +0530

    checking code

--- social-media/src/store/Post-list-store.jsx
+++ social-media/src/store/Post-list-store.jsx
@@ -1,66 +1,66 @@
-import { createContext, useCallback, useReducer,useState,useEffect } from "react";
+import {
+  createContext,
+  useCallback,
+  useReducer,
+  useState,
+  useEffect,
+} from "react";
 
-export const Postlist=createContext({
-  postlist:[],
-  addpost:()=>{},
-  deletepost:()=>{},
+export const Postlist = createContext({
+  postList: [],
+  addpost: () => {},
+  addInitialposts: () => {},
+  deletepost: () => {},
 });
-const postlistReducer=(currpostlist,action)=>{
-  let newpostlist=currpostlist;
-  if (action.type==="DELETE_POST"){
-    newpostlist=currpostlist.filter((post)=>post.id !==action.payload.postid);
+const postlistReducer = (currpostlist, action) => {
+  let newpostlist = currpostlist;
+  if (action.type === "DELETE_POST") {
+    newpostlist = currpostlist.filter(
+      (post) => post.id !== action.payload.postid
+    );
+  } else if (action.type === "ADD_INITIAL_POSTS") {
+    newpostlist = action.payload.posts;
+  } else if (action.type === "ADD_POST") {
+    newpostlist = [action.payload.post, ...currpostlist];
   }
-  else if(action.type==="ADD_INITIAL_POSTS"){
-    newpostlist=action.payload.posts;
-  }
-  else if(action.type==="ADD_POST"){
-newpostlist=[action.payload.post,...currpostlist]
-   } 
   return newpostlist;
 };
 
+const Postlistprovider = ({ children }) => {
+  const [postList, dispatchpostlist] = useReducer(postlistReducer, []);
 
-const Postlistprovider=({children})=>{
-const [postlist,dispatchpostlist]=useReducer(postlistReducer,[]);
-
-
-
-  const addpost=(post)=>{
+  const addpost = (post) => {
     dispatchpostlist({
-      type:"ADD_POST",
-      payload:post, 
-    })
-    };
-   
-    const addInitialposts=(posts)=>{
-      dispatchpostlist({
-        type:"ADD_INITIAL_POSTS",
-        payload:{
+      type: "ADD_POST",
+      payload: { post },
+    });
+  };
+
+  const addInitialposts = (posts) => {
+    dispatchpostlist({
+      type: "ADD_INITIAL_POSTS",
+      payload: {
         posts,
-        },
-      });
-      };
-
-    const deletepost=useCallback((postid)=>{
-      dispatchpostlist({
-        type:"DELETE_POST",
-        payload:{postid,},
-      });
       },
-      [dispatchpostlist]
-    );
-      
+    });
+  };
 
-      
- 
+  const deletepost = useCallback(
+    (postid) => {
+      dispatchpostlist({
+        type: "DELETE_POST",
+        payload: { postid },
+      });
+    },
+    [dispatchpostlist]
+  );
 
-return <Postlist.Provider value={
- { postlist,
-  addpost,
-  deletepost}
-}>
-{children}
-</Postlist.Provider>
-}
+  return (
+    <Postlist.Provider
+      value={{ postList, addpost, deletepost, addInitialposts }}
+    >
+      {children}
+    </Postlist.Provider>
+  );
+};
 export default Postlistprovider;
-
