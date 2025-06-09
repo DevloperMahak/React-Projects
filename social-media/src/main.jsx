bc5976344197154a99670be733967a485b5f3477
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-06-09 20:08:12 +0530

    correcting post-list path

--- social-media/src/main.jsx
+++ social-media/src/main.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './router/App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {RouterProvider,createBrowserRouter} from "react-router-dom";
-import Createpost, { createpostaction } from './components/Createpost.jsx';
-import  Postlist from './store/Post-list-store.jsx';
-import { postloader } from './components/Postlist.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./router/App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Createpost, { createpostaction } from "./components/Createpost.jsx";
+import Postlist from "./components/Postlist.jsx";
+import { postLoader } from "./components/Postlist.jsx";
 
-const router=createBrowserRouter([
-{
-  path:"/",
-   element:<App/>, 
-   children:[
-  {path:"/Post-list", element:<Postlist/>,loader:postloader},
-  {path:"/create-post", element: <Createpost/>, action:createpostaction,}
-],
-},
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/Post-list", element: <Postlist />, loader: postLoader },
+      {
+        path: "/create-post",
+        element: <Createpost />,
+        action: createpostaction,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
